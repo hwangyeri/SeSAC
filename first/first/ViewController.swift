d721f6366d1651be2c23ff55f7728cb0032b1cea
Yeri Hwang <114602459+hwangyeri@users.noreply.github.com>
2023-08-01 21:28:16 +0900

    230718 first Project 과제 제출

---
+++ first/first/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first
+//
+//  Created by 황예리 on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
