7c4464e919dcbdf8fbbbb7e47a9ffaa2fa984a26
hwangyeri <dpfl420@naver.com>
2023-08-12 17:09:53 +0900

    Media Project 및.gitignore File 생성

---
+++ Media/Media/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Media
+//
+//  Created by 황예리 on 2023/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
