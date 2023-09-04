a8ccfb0c54eb76af23a58d46ecaa72095f6190ce
hwangyeri <dpfl420@naver.com>
2023-09-05 00:05:28 +0900

    [Fix] 검색 결과 화면에 띄우기, CollectionView Refactoring

--- BOOKWARM/BOOKWARM/Scene/Library/MySearchViewController.swift
+++ BOOKWARM/BOOKWARM/Scene/Library/MySearchViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class MySearchViewController: UIViewController, UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
+class MySearchViewController: UIViewController, UISearchBarDelegate {
     
-    static let identifier = "MySearchViewController"
     let movieInfo = MovieInfo()
+    
     lazy var movieTitles = movieInfo.getMovieTitles()
     
-    @IBOutlet var searchCollectionView: UICollectionView!
+    @IBOutlet var collectionView: UICollectionView!
     
     let searchBar = UISearchBar()
     let placeholderText = "검색어를 입력해주세요!"
@@ -23,15 +23,18 @@ class MySearchViewController: UIViewController, UISearchBarDelegate, UICollectio
         
         super.viewDidLoad()
         
+        collectionView.dataSource = self
+        collectionView.delegate = self
         searchBar.delegate = self
-        
         
         searchBar.placeholder = placeholderText
         searchBar.showsCancelButton = true
         navigationItem.titleView = searchBar
         
-        let nib = UINib(nibName: "MySearchViewController", bundle: nil)
-        //collectionView.register(nib, forCellWithReuseIdentifier: "SearchCollectionViewCell")
+        configureViewLayout()
+        
+        let nib = UINib(nibName: MySearchCollectionViewCell.reuseIdentifier, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: MySearchCollectionViewCell.reuseIdentifier)
         
         let xmark = UIImage(systemName: "xmark")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: xmark, style: .plain, target: self, action: #selector(closeButtonClicked))
@@ -54,15 +57,14 @@ class MySearchViewController: UIViewController, UISearchBarDelegate, UICollectio
                 print(searchResultList)
             }
         }
-        //reload
         
+        collectionView.reloadData()
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         
         searchResultList.removeAll()
         searchBar.text = ""
-        
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -74,20 +76,47 @@ class MySearchViewController: UIViewController, UISearchBarDelegate, UICollectio
             }
         }
         
-        //reload
+        collectionView.reloadData()
     }
     
-    // MARK: - collectionView
+    
+}
+
+extension MySearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return searchResultList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MySearchViewController", for: indexPath) as! MySearchCollectionViewCell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MySearchCollectionViewCell.reuseIdentifier, for: indexPath) as? MySearchCollectionViewCell else { return UICollectionViewCell() }
         cell.mainTitleLable.text = movieInfo.movie[indexPath.row].title
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = storyboard?.instantiateViewController(identifier: DetailViewController.reuseIdentifier) as! DetailViewController
+        let selectedMovie = movieInfo.movie[indexPath.row]
+        
+        vc.selectedMovie = selectedMovie
+        vc.naviTitle = movieInfo.movie[indexPath.row].title
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func configureViewLayout() {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 20
+        let width = UIScreen.main.bounds.width - (spacing * 2)
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: width, height: width)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+
+        collectionView.collectionViewLayout = layout
+        collectionView.isPrefetchingEnabled = true
     }
     
     
