40b13e1022e458c1789035bba70c0f5d1733e8d7
hwangyeri <dpfl420@naver.com>
2023-11-06 00:15:02 +0900

    [Feat] 셀 클릭시 화면 전환되는 상세 화면 생성 및 초기 설정

---
+++ Setting/Setting/ShoppingList/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Setting
+//
+//  Created by Yeri Hwang on 2023/11/05.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var selectedItem: ShoppingListItem?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+    
+
+}
