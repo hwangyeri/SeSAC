1f3d94d47371c7484e90067c5e1ce253a0a88764
hwangyeri <dpfl420@naver.com>
2023-11-07 01:27:06 +0900

    [Feat] iTunesDetailViewController 생성

---
+++ SeSACRxThreads/SeSACRxThreads/iTunes/iTunesDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  iTunesDetailViewController.swift
+//  SeSACRxThreads
+//
+//  Created by Yeri Hwang on 2023/11/06.
+//
+
+import UIKit
+
+class iTunesDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
