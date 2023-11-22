1d698862be49ba2324198c2121ad525953bd8711
hwangyeri <dpfl420@naver.com>
2023-11-22 14:16:35 +0900

    [Feat] Animation 그룹으로 폴더링 및 WalletModel 생성

---
+++ SwiftUICoinOrderbook/SwiftUICoinOrderbook/Animation/WalletModel.swift
@@ -0,0 +1,21 @@
+//
+//  WalletModel.swift
+//  SwiftUICoinOrderbook
+//
+//  Created by Yeri Hwang on 2023/11/22.
+//
+
+import SwiftUI
+
+struct WalletModel: Hashable {
+    let color = Color.random()
+    let name: String
+    let index: Int
+}
+
+var walletList = [
+    WalletModel(name: "Hue Card", index: 0),
+    WalletModel(name: "Jack Card", index: 1),
+    WalletModel(name: "Bran Card", index: 2),
+    WalletModel(name: "Koko Card", index: 3)
+]
